4001ef0e196f776e1abbf6c259bd309b239f25e2
MaxCobMara <maxcobmara@gmail.com>
2013-08-30 00:08:58 +0800

    update for Rails 4/2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skeleton::Application.config.secret_key_base = '47b451ad0b9514f316194d7325922624000587c014fc85371f084b56b28e33a88ebd140d46f4d696af7847b30831d9fa99f955c43ecec1dbea941567437da4f4'
--- db/migrate/20110420090050_add_column_to_roles.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnToRoles < ActiveRecord::Migration
-  def self.up
-    add_column :roles, :authname, :string
-  end
-
-  def self.down
-    remove_column :roles, :authname
-  end
-end
